78de6965715da279dfbd90eac2c0db6493bfde47
CEKlopfenstein <carlstrain1@gmail.com>
2024-02-18 20:10:25 +0000

    Reworked Demo/Example Into "Working" Version
    * Removed .vscode (accidently commited previously.)
    * Updated Plugin Info
    * Added Config Options
        * Discord Webhook URL, Gotify Client Token, and Gotify Server URL now saved in Config.
        * Required Config values are validated with error messages.
        * Default Config Values Added (Only Gotify Server URL is populated at the start)
    * Updated Plugin Info
        * Version Numbers will be as follows YEAR.RELEASE.COMMITS_SINCE_LAST_RELEASE
    * Repeater Functionality
        * Functions to Start and Stop the Repeater are now implemented/moved out of Enable Function.
        * Repeater will attempt to make a connection x times before determining it has failed.
        * Disabling the plugin will cause it to disconnect
    * Discord Message Sent Now has the Title as a Header and the Message below it.
    * Updated README.md moved old on to OG_README.md for reference.
    * Displayer now contains information on how to configure the plugin.

--- README.md
+++ README.md
@@ -1,29 +1,26 @@
-# gotify/plugin-template [![](https://travis-ci.org/gotify/plugin-template.svg?branch=master)](https://travis-ci.org/gotify/plugin-template)
+# CEKlopfenstein/gotify-repeater
 
-A plugin template for [gotify/server](https://github.com/gotify/server)
-using [gotify/plugin-api](https://github.com/gotify/plugin-api).
+A plugin for [gotify/server](https://github.com/gotify/server) using [gotify/plugin-template](https://github.com/gotify/plugin-template) as a base.
 
-## Getting Started
+The goal of this plugin is to enable Gotify to send out notifications through services beyond just it's own clients. Initially this will be Discord Webhooks with farther Expanstion in the future.
 
-1. Clone, fork or copy this repository.
-1. Change `PLUGIN_NAME` in [Makefile](Makefile).
-1. Setup building on every release
-   * Enable travis-ci in your repository.
-   * Add `GH_TOKEN` environment variable see [travis-ci docs](https://docs.travis-ci.com/user/deployment/pages/#setting-the-github-token).
-1. Implement your plugin. See [plugin docs](https://gotify.net/docs/plugin).
-1. Create a release to automatically build the plugin.
+## But why?
+I have a few things that support Gotify for notifications but not something else I use. That and I'd like a centralized places to direct all the notifications within my homelab. Is this the best method? Who knows. But let's have fun doing it.
 
-*When you're done, feel free to add your plugin to [gotify/contrib](https://github.com/gotify/contrib).*
+## Plans (In order of priority)
+- [ ] Basic Discord Webhook integration
+- [ ] User Friendly configuration
+- [ ] Embedded Discord Webhook integration
+- [ ] Automated Releases
+- [ ] More integrations
+   - Telegram
+   - Pushbullet
+   - Discord bot? (likely more complex than needed bug hey. I'm spit balling here.)
 
 ## Building
+For now please refer to [OG_README.md](OG_README.md) for documentation on how to build.
 
-For building the plugin gotify/build docker images are used to ensure compatibility with 
-[gotify/server](https://github.com/gotify/server).
+Please note that modifications have been made to the make file to allow it to function on my machine spesifically. They may not function else where. I plan to eventually remedy that.
 
-`GOTIFY_VERSION` can be a tag, commit or branch from the gotify/server repository.
-
-This command builds the plugin for amd64, arm-7 and arm64. 
-The resulting shared object will be compatible with gotify/server version 2.0.20.
-```bash
-$ make GOTIFY_VERSION="v2.0.20" FILE_SUFFIX="for-gotify-v2.0.20" build
-```
+## Trial Running
+The Makefile has had an extra command added to it to allow for the automated running of a [gotify/server](https://github.com/gotify/server) with the plugin install. To access run `make run`. Note as does the origial Makefile it also requires the installation of docker. It will also use `sudo` to attempt to run docker. You have been warned.
--- plugin.go
+++ plugin.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
 	"net/url"
@@ -12,99 +13,204 @@ import (
 	"github.com/gotify/plugin-api"
 )
 
+var info = plugin.Info{
+	ModulePath:  "github.com/CEKlopfenstein/gotify-repeater",
+	Version:     "2024.0.4",
+	Author:      "CEKlopfenstein",
+	Description: "A simple Plugin that provides the ability to pass notifications recieved throught to discord. (Current Implementation. More planned.)",
+	Name:        "Gotify Repeater",
+}
+
 // GetGotifyPluginInfo returns gotify plugin info.
 func GetGotifyPluginInfo() plugin.Info {
-	return plugin.Info{
-		ModulePath:  "github.com/gotify/plugin-template",
-		Version:     "1.0.0",
-		Author:      "CEKlopfenstein",
-		Description: "An example plugin with travis-ci building",
-		Name:        "cekwebhooks",
-	}
+	return info
 }
 
 // MyPlugin is the gotify plugin instance.
 type MyPlugin struct {
-	userCtx     plugin.UserContext
-	msgHandler  plugin.MessageHandler
-	serverURL   string
-	clientToken string
+	userCtx    plugin.UserContext
+	msgHandler plugin.MessageHandler
+	config     *Config
+	listener   *websocket.Conn
+}
+
+func (c *MyPlugin) StartRepeater() {
+	var attemptTick = -1
+	var attemptLimit = 100
+	log.Println("Repeater Attempting to Connect")
+	for {
+		if attemptTick >= attemptLimit {
+			log.Println("Repeater Failed to Connect to Server due to refused connection (Slow Start Up Likely)")
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+		attemptTick++
+		health, err := url.Parse(c.config.ServerURL)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		health.Path = "/version"
+		resp, err := http.Get(health.String())
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			log.Println(resp.Status)
+			continue
+		}
+		break
+	}
+
+	u, err := url.Parse(c.config.ServerURL)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	u.Path = "/stream"
+	query := u.Query()
+	query.Add("token", c.config.ClientToken)
+	u.RawQuery = query.Encode()
+	switch u.Scheme {
+	case "http":
+		u.Scheme = "ws"
+	case "https":
+		u.Scheme = "wss"
+	default:
+		log.Println("Invalid Scheme for URL")
+		return
+	}
+
+	ws, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	c.listener = ws
+	log.Println("Repeater Connected")
+	go func() {
+		for {
+			_, message, err := ws.ReadMessage()
+			if err != nil && c.listener != nil {
+				log.Println("Error Reading Message", err)
+				return
+			} else if c.listener == nil {
+				return
+			}
+
+			type test struct {
+				Appid    int
+				Date     string
+				Extras   []byte
+				Id       int
+				Message  string
+				Title    string
+				Priority int
+			}
+			data := test{}
+			json.Unmarshal(message, &data)
+			type discordhook struct {
+				Content string `json:"content"`
+			}
+			var discordSend = discordhook{Content: "# " + data.Title + "\n\n" + data.Message}
+			byteData, err := json.Marshal(&discordSend)
+			if err != nil {
+				log.Println(err.Error())
+			}
+			resp, err := http.Post(c.config.DiscordWebHook, "application/json", bytes.NewReader(byteData))
+			if err != nil {
+				log.Println(err.Error())
+			} else if resp.StatusCode != http.StatusNoContent {
+				log.Println(resp.Status)
+			}
+		}
+	}()
+}
+
+func (c *MyPlugin) StopRepeater() {
+	if c.listener != nil {
+		listener := c.listener
+		c.listener = nil
+		listener.Close()
+	}
 }
 
 // Enable enables the plugin.
 func (c *MyPlugin) Enable() error {
-	go func() {
-		time.Sleep(5 * time.Second)
-		log.Println("Connecting to", c.serverURL)
-		ws, _, err := websocket.DefaultDialer.Dial("ws://localhost:80/stream?token="+c.clientToken, nil)
-		if err != nil {
-			log.Println(err.Error())
-		}
-
-		go func() {
-			for {
-				mt, message, err := ws.ReadMessage()
-				if err != nil {
-					log.Println("read:", err)
-					return
-				}
-				log.Printf("recv: %s, type: %d", message, mt)
-				type test struct {
-					Appid    int
-					Date     string
-					Extras   []byte
-					Id       int
-					Message  string
-					Title    string
-					Priority int
-				}
-				data := test{}
-				json.Unmarshal(message, &data)
-				log.Println(data.Title, data.Message)
-				log.Println(data)
-				var postURL = ""
-				type discordhook struct {
-					Content string `json:"content"`
-				}
-				var discordSend = discordhook{Content: data.Message}
-				log.Println("One", discordSend)
-				byteData, err := json.Marshal(&discordSend)
-				if err != nil {
-					log.Println(err.Error())
-				}
-				log.Println("Two", string(byteData))
-				resp, err := http.Post(postURL, "application/json", bytes.NewReader(byteData))
-				if err != nil {
-					log.Println(err.Error())
-				} else if resp.StatusCode != http.StatusNoContent {
-					log.Println(resp.Status)
-				}
-			}
-		}()
-		time.Sleep(5 * time.Second)
-		c.msgHandler.SendMessage(plugin.Message{
-			Message: "The plugin has been enabled for 5 seconds.",
-		})
-	}()
+	go c.StartRepeater()
 	return nil
 }
 
 // Disable disables the plugin.
 func (c *MyPlugin) Disable() error {
+	c.StopRepeater()
 	return nil
 }
 
 func (c *MyPlugin) GetDisplay(location *url.URL) string {
 	var toReturn = ""
 
-	if c.userCtx.Admin {
-		toReturn += "Greetings Administrator "
-	} else {
-		toReturn += "Greatings "
-	}
-	toReturn += c.userCtx.Name + "\n"
+	toReturn += "Version: " + info.Version + "\n\nDescription: " + info.Description + "\n\n"
 
+	toReturn += "In order to have this plugin function correctly 3 values are needed within. `discordwebhook`, `clienttoken`, and `serverurl`.\n\n`serverurl` can often be left as the default. Unless you enable HTTPS or wish to have the the plugin listen through some other URL. Note this can allow you to have the plugin listen to a different server entirely. This is not advised. As reconnection after a lost connection is not attempted at this time.\n\n`clienttoken` is the client the plugin will connect as. This can be any client you desire. It would be advisable to create it's own client in the Client Menu.\n\n`discordwebhool` is the webhook the plugin will use to send out messages. Currently the name is not modified by the plugin. So the username will be what ever it was set to at creation."
 	return toReturn
+}
+
+type Config struct {
+	DiscordWebHook string
+	ClientToken    string
+	ServerURL      string
+}
+
+// Set Default Values of Config
+func (c *MyPlugin) DefaultConfig() interface{} {
+	return &Config{
+		DiscordWebHook: "",
+		ClientToken:    "",
+		ServerURL:      "http://localhost",
+	}
+}
+
+func (c *MyPlugin) ValidateAndSetConfig(cd interface{}) error {
+	config := cd.(*Config)
+	// Validation of Discord Webhook
+	if len(config.DiscordWebHook) == 0 {
+		return errors.New("discord Webhook required")
+	} else {
+		resp, err := http.Get(config.DiscordWebHook)
+		if err != nil {
+			return errors.Join(errors.New("discord Webhook invalid"), err)
+		} else if resp.StatusCode != http.StatusOK {
+			return errors.New("discord Webhook invalid. Discord returned value other than success")
+		}
+	}
+
+	// Validation of local server URL
+	if len(config.ServerURL) == 0 {
+		return errors.New("server url invalid")
+	} else {
+		u, err := url.Parse(config.ServerURL)
+		if err != nil {
+			return errors.Join(errors.New("server url invalid"), err)
+		}
+		switch u.Scheme {
+		case "http":
+		case "https":
+		default:
+			return errors.New("server URL invalid URL must be HTTP or HTTPS")
+		}
+		if len(u.Path) > 0 {
+			return errors.New("server URL invalid URL must not include a path")
+		}
+	}
+
+	if len(config.ClientToken) == 0 {
+		return errors.New("client token required")
+	}
+	c.config = config
+	return nil
 }
 
 func (c *MyPlugin) SetMessageHandler(h plugin.MessageHandler) {
@@ -113,7 +219,7 @@ func (c *MyPlugin) SetMessageHandler(h plugin.MessageHandler) {
 
 // NewGotifyPluginInstance creates a plugin instance for a user context.
 func NewGotifyPluginInstance(ctx plugin.UserContext) plugin.Plugin {
-	return &MyPlugin{userCtx: ctx, serverURL: "http://localhost/", clientToken: "Ct7UrVHyPQRuwcp"}
+	return &MyPlugin{userCtx: ctx}
 }
 
 func main() {
