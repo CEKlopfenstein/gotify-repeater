5672ee33347e6ccd1afa2c356a579b87e1964843
CEKlopfenstein <carlstrain1@gmail.com>
2024-02-18 04:07:04 +0000

    Working Demo/Test of Repeating to External Webhook
    * Removed Unneeded port in docker-compose
    * Set up demo/test with hardcoded values to test general idea.

--- plugin.go
+++ plugin.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
 	"net/url"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/gotify/plugin-api"
 )
 
@@ -20,13 +25,62 @@ func GetGotifyPluginInfo() plugin.Info {
 
 // MyPlugin is the gotify plugin instance.
 type MyPlugin struct {
-	userCtx    plugin.UserContext
-	msgHandler plugin.MessageHandler
+	userCtx     plugin.UserContext
+	msgHandler  plugin.MessageHandler
+	serverURL   string
+	clientToken string
 }
 
 // Enable enables the plugin.
 func (c *MyPlugin) Enable() error {
 	go func() {
+		time.Sleep(5 * time.Second)
+		log.Println("Connecting to", c.serverURL)
+		ws, _, err := websocket.DefaultDialer.Dial("ws://localhost:80/stream?token="+c.clientToken, nil)
+		if err != nil {
+			log.Println(err.Error())
+		}
+
+		go func() {
+			for {
+				mt, message, err := ws.ReadMessage()
+				if err != nil {
+					log.Println("read:", err)
+					return
+				}
+				log.Printf("recv: %s, type: %d", message, mt)
+				type test struct {
+					Appid    int
+					Date     string
+					Extras   []byte
+					Id       int
+					Message  string
+					Title    string
+					Priority int
+				}
+				data := test{}
+				json.Unmarshal(message, &data)
+				log.Println(data.Title, data.Message)
+				log.Println(data)
+				var postURL = ""
+				type discordhook struct {
+					Content string `json:"content"`
+				}
+				var discordSend = discordhook{Content: data.Message}
+				log.Println("One", discordSend)
+				byteData, err := json.Marshal(&discordSend)
+				if err != nil {
+					log.Println(err.Error())
+				}
+				log.Println("Two", string(byteData))
+				resp, err := http.Post(postURL, "application/json", bytes.NewReader(byteData))
+				if err != nil {
+					log.Println(err.Error())
+				} else if resp.StatusCode != http.StatusNoContent {
+					log.Println(resp.Status)
+				}
+			}
+		}()
 		time.Sleep(5 * time.Second)
 		c.msgHandler.SendMessage(plugin.Message{
 			Message: "The plugin has been enabled for 5 seconds.",
@@ -59,7 +113,7 @@ func (c *MyPlugin) SetMessageHandler(h plugin.MessageHandler) {
 
 // NewGotifyPluginInstance creates a plugin instance for a user context.
 func NewGotifyPluginInstance(ctx plugin.UserContext) plugin.Plugin {
-	return &MyPlugin{userCtx: ctx}
+	return &MyPlugin{userCtx: ctx, serverURL: "http://localhost/", clientToken: "Ct7UrVHyPQRuwcp"}
 }
 
 func main() {
